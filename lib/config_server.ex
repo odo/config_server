9aaef5bb0b6f2fc4fcd1c89d1344184d2cebd1e3
Florian Odronitz <odo@mac.com>
2026-05-12 18:04:53 +0200

    making sure we can cold start with no existing directory

--- lib/config_server.ex
+++ lib/config_server.ex
@@ -7,7 +7,6 @@ defmodule ConfigServer do
 
   defstruct [
     :repo_url,
-    :github_token,
     :repo_path,
     :config,
     :commit_hash,
@@ -22,20 +21,18 @@ defmodule ConfigServer do
   # Server functions
   @spec start_link(%{
       repo_url: String.t(),
-      github_token: String.t(),
       repo_path: String.t(),
       pull_interval_ms: integer(),
     }) :: {:ok, pid()}
-  def start_link(%{repo_url: _, github_token: _, repo_path: _, pull_interval_ms: _}) do
-    {:ok, _} = GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  def start_link(%{repo_url: _, repo_path: _, pull_interval_ms: _} = args) do
+    {:ok, _} = GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
   @impl true
-  def init(%{repo_url: repo_url, github_token: github_token, repo_path: repo_path, pull_interval_ms: pull_interval_ms})
-    when is_binary(repo_url) and is_binary(github_token) and is_binary(repo_path) and is_integer(pull_interval_ms) do
+  def init(%{repo_url: repo_url, repo_path: repo_path, pull_interval_ms: pull_interval_ms})
+    when is_binary(repo_url) and is_binary(repo_path) and is_integer(pull_interval_ms) do
     initial_state = %ConfigServer{
       repo_url: repo_url,
-      github_token: github_token,
       repo_path: repo_path,
       pull_interval_ms: pull_interval_ms
     }
@@ -52,7 +49,7 @@ defmodule ConfigServer do
   @impl true
   def handle_info(:pull_configs, state) do
     schedule_next_pull(state)
-    pull_configs_from_repo(state)
+    spawn(fn() -> pull_configs_from_repo(state) end)
 
     {:noreply, state}
   end
@@ -72,26 +69,23 @@ defmodule ConfigServer do
     }
   end
 
-  defp pull_configs_from_repo(%ConfigServer{repo_path: repo_path, github_token: github_token, repo_url: repo_url}) do
-    config_server = self()
-    spawn(fn () ->
-      if !File.exists?(repo_path) do
+  defp pull_configs_from_repo(%ConfigServer{repo_path: repo_path, repo_url: repo_url}) do
+    if !File.exists?(repo_path) do
         Logger.info("Setting up folder for config repo")
         {_, 0} = System.cmd("mkdir", ["-p", repo_path])
       end
 
       git_path = Path.join(repo_path, ".git")
-      case !File.exists?(git_path) do
+      case File.exists?(git_path) do
         true ->
           Logger.info("Pulling config repo")
           {_, 0} = System.cmd("git", ["pull"], cd: repo_path)
         false ->
           Logger.info("Cloning config repo")
-          {_, 0} = System.cmd("git", ["clone", "https://#{github_token}@" <> repo_url, repo_path])
+          {_, 0} = System.cmd("git", ["clone", repo_url, repo_path])
       end
 
-      GenServer.cast(config_server, :parse_configs)
-    end)
+      GenServer.cast(__MODULE__, :parse_configs)
   end
 
   def schedule_next_pull(%ConfigServer{pull_interval_ms: pull_interval_ms}) do

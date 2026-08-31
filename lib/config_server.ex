ea6fa270d3e33940338883ddc78e19532232bbbd
Florian Odronitz <odo@mac.com>
2026-08-31 23:21:41 +0200

    adding branch option and rollabacks

--- lib/config_server.ex
+++ lib/config_server.ex
@@ -13,6 +13,7 @@ defmodule ConfigServer do
     :repo_url,
     :git_ssh_command,
     :repo_path,
+    :branch,
     :config,
     :commit_hash,
     :pull_interval_ms,
@@ -37,13 +38,15 @@ defmodule ConfigServer do
 
   @spec start_link(%{
       repo_url: String.t(),
+      branch: String.t() | nil,
       git_ssh_command: String.t() | nil,
       repo_path: String.t(),
       pull_interval_ms: integer(),
       state_change_fun: nil | fun()
     }) :: {:ok, pid()}
-  def start_link(%{repo_url: repo_url, repo_path: repo_path, git_ssh_command: git_ssh_command, pull_interval_ms: pull_interval_ms, state_change_fun: state_change_fun} = args)
+  def start_link(%{repo_url: repo_url, repo_path: repo_path, branch: branch, git_ssh_command: git_ssh_command, pull_interval_ms: pull_interval_ms, state_change_fun: state_change_fun} = args)
     when is_binary(repo_url) and is_binary(repo_path) and
+         (is_binary(branch) or is_nil(branch)) and
          (is_binary(git_ssh_command) or is_nil(git_ssh_command)) and
          is_integer(pull_interval_ms) and pull_interval_ms > 0 and
          (is_nil(state_change_fun) or is_function(state_change_fun) or is_tuple(state_change_fun)) do
@@ -51,13 +54,17 @@ defmodule ConfigServer do
   end
 
   @impl true
-  def init(%{repo_url: repo_url, repo_path: repo_path, git_ssh_command: git_ssh_command, pull_interval_ms: pull_interval_ms, state_change_fun: state_change_fun})
+  def init(%{repo_url: repo_url, repo_path: repo_path, branch: branch, git_ssh_command: git_ssh_command, pull_interval_ms: pull_interval_ms, state_change_fun: state_change_fun})
     when is_binary(repo_url) and is_binary(repo_path) and
+         (is_binary(branch) or is_nil(branch)) and
          (is_binary(git_ssh_command) or is_nil(git_ssh_command)) and
          is_integer(pull_interval_ms) and pull_interval_ms > 0 and
          (is_nil(state_change_fun) or is_function(state_change_fun) or is_tuple(state_change_fun)) do
+    {:ok, _commit_hash} = Git.ensure_checkout(repo_url, repo_path, git_ssh_command)
+    branch = branch || Git.default_branch(repo_path, git_ssh_command)
     initial_state = %ConfigServer{
       repo_url: repo_url,
+      branch: branch,
       git_ssh_command: git_ssh_command,
       repo_path: repo_path,
       pull_interval_ms: pull_interval_ms,
@@ -72,11 +79,11 @@ defmodule ConfigServer do
   def handle_info(
     :pull_configs,
     %ConfigServer{
-      repo_url: repo_url, git_ssh_command: git_ssh_command, repo_path: repo_path,
+      repo_url: repo_url, branch: branch, git_ssh_command: git_ssh_command, repo_path: repo_path,
       pull_interval_ms: pull_interval_ms, state_change_fun: state_change_fun, commit_hash: commit_hash} = state
   ) do
     schedule_next_pull(pull_interval_ms)
-    case Git.refresh(repo_url, repo_path, git_ssh_command) do
+    case Git.refresh(repo_path, branch, git_ssh_command) do
       {:ok, ^commit_hash} ->
         {:noreply, state}
       {:ok, next_commit_hash} ->
@@ -87,9 +94,22 @@ defmodule ConfigServer do
             commit_hash: next_commit_hash 
           }
         :ets.insert(__MODULE__, {:config, next_state.config})
-        execute(state_change_fun, state.config, next_state.config)
-        {:noreply, next_state}
-      {:error, _} ->
+        case execute(state_change_fun, state.config, next_state.config) do
+          :ok ->
+            {:noreply, next_state}
+          {:error, error} ->
+            case commit_hash do
+              nil ->
+                Logger.error("Config callback returned error (no known state to roll back to) : #{inspect(error)}")
+                throw({:error, :no_known_good_state})
+              _ ->
+                Logger.error("Config callback returned error (rolling back to #{commit_hash}) : #{inspect(error)}")
+                {:ok, ^commit_hash} = Git.checkout(commit_hash, repo_path, git_ssh_command)
+                {:noreply, state}
+            end
+        end
+      {:error, error} ->
+        Logger.warning("Failed to pull repo #{repo_url}: #{inspect(error)}")
         {:noreply, state}
     end
   end
@@ -101,18 +121,36 @@ defmodule ConfigServer do
   def get_application_env() do
     %{
       repo_url: Application.get_env(:config_server, :repo_url), 
+      branch: Application.get_env(:config_server, :branch), 
       git_ssh_command: Application.get_env(:config_server, :git_ssh_command), 
       repo_path: Application.get_env(:config_server, :repo_path),
       pull_interval_ms: Application.get_env(:config_server, :pull_interval_ms), 
       state_change_fun: Application.get_env(:config_server, :state_change_fun)
     }
+    |> validate_env([:repo_url, :repo_path, :pull_interval_ms, :state_change_fun])
   end
-  
-  defp execute({module, function}, old_config, new_config) do
-      apply(module, function, [old_config, new_config])
+ 
+  defp validate_env(map, keys) do
+    case Enum.all?(keys, &(Map.get(map, &1) != nil)) do
+      true -> map
+      false -> throw({:error, {:required_config, keys}})
+    end
   end
 
-  defp execute(state_change_fun, old_config, new_config) when is_function(state_change_fun) do
+  defp execute(callback, old_config, new_config) do
+    case do_execute(callback, old_config, new_config) do
+      :error -> {:error, :unknown}
+      {:error, error} -> {:error, error}
+      _ -> :ok
+    end
+  rescue
+    error -> {:error, error}
+  end
+
+  defp do_execute({module, function}, old_config, new_config) do
+    apply(module, function, [old_config, new_config])
+  end
+  defp do_execute(state_change_fun, old_config, new_config) when is_function(state_change_fun) do
       state_change_fun.(old_config, new_config)
   end
 
--- lib/config_server/git.ex
+++ lib/config_server/git.ex
@@ -2,31 +2,29 @@ defmodule ConfigServer.Git do
 
   require Logger
 
-  def refresh(repo_url, repo_path, git_ssh_command) do 
+  def refresh(repo_path, branch, git_ssh_command) do 
+    {:ok, _} = checkout(branch, repo_path, git_ssh_command)
+    case System.cmd("git", ["pull"], cd: repo_path, env: env(git_ssh_command)) do
+    {_, 0} ->
+        {:ok, commit_hash(repo_path)} 
+    {_, exit_code} ->
+        Logger.error("Failed to pull repo (exit code #{exit_code})")
+        {:error, :failed_to_pull}
+    end
+  end
+
+  def ensure_checkout(repo_url, repo_path, git_ssh_command) do
     if !File.exists?(repo_path) do
       Logger.info("Setting up folder for config repo")
       {_, 0} = System.cmd("mkdir", ["-p", repo_path])
     end
 
-    env =
-    case git_ssh_command do
-        nil -> []
-        command -> [{"GIT_SSH_COMMAND", command}]
-      end
-
     git_path = Path.join(repo_path, ".git")
     case File.exists?(git_path) do
       true ->
-        case System.cmd("git", ["pull"], cd: repo_path, env: env) do
-        {_, 0} ->
-            {:ok, commit_hash(repo_path)} 
-        {_, exit_code} ->
-            Logger.error("Failed to pull repo (exit code #{exit_code})")
-            {:error, :failed_to_pull}
-        end
-      false ->
-        Logger.info("Cloning config repo from #{repo_path} into #{repo_path}")
-        case System.cmd("git", ["clone", repo_url, repo_path], env: env) do
+        {:ok, commit_hash(git_path)}
+      false -> Logger.info("Cloning config repo from #{repo_url} into #{repo_path}")
+        case System.cmd("git", ["clone", repo_url, repo_path], env: env(git_ssh_command)) do
         {_, 0} ->
             {:ok, commit_hash(repo_path)} 
         {_, exit_code} ->
@@ -36,8 +34,22 @@ defmodule ConfigServer.Git do
     end
   end
 
+  def checkout(commit_hash, repo_path, git_ssh_command) do
+    {_, 0} = System.cmd("git", ["checkout", commit_hash], cd: repo_path, env: env(git_ssh_command), stderr_to_stdout: true)
+    {:ok, commit_hash}
+  end
+
+  def default_branch(repo_path, git_ssh_command) do
+    {reply, 0} = System.cmd("git", ["remote", "show", "origin"], cd: repo_path, env: env(git_ssh_command))
+    [[[_, branch]]] = reply |> String.split("\n") |> Enum.map(&String.trim(&1)) |> Enum.map(&Regex.scan(~r/HEAD branch: (.*)/, &1)) |> Enum.filter(&(&1!=[]))
+    branch
+  end
+
   defp commit_hash(repo_path) do
     {commit_hash, 0} = System.cmd("git", ["log", "-1", "--format=%H"], cd: repo_path)
     commit_hash |> String.trim
   end
+
+  defp env(nil), do: []
+  defp env(command), do: [{"GIT_SSH_COMMAND", command}]
 end
